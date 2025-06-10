9f487c78c0fd65cc9630671fa9c66a22b8308057
ibrahimsrgn <101056719+ibrahimsrgn@users.noreply.github.com>
2025-06-10 13:52:20 +0300

    Player can aim

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private Transform fireRod;
+    private void Update()
+    {
+        // Fare pozisyonunu dünya koordinatlarına çeviriyoruz
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 2D ortamda sadece z eksenini dikkate almak için z'yi sıfırlıyoruz
+        mousePosition.z = 0;
+
+        // Dönüşüm için yön vektörünü hesaplıyoruz
+        Vector2 lookDir = (mousePosition - fireRod.position).normalized;
+
+        // Yön vektörünü kullanarak dönüşümü uyguluyoruz
+        fireRod.rotation = Quaternion.FromToRotation(Vector2.right, lookDir);
+    }
+}
